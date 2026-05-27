0444f2aa1d1aac5efb02c5cec33bf6efa86aeb69
0siriz <0siriz@proton.me>
2026-05-27 18:54:51 +0200

    Don't resize on font increase

--- wezterm.lua
+++ wezterm.lua
@@ -115,6 +115,7 @@ config.tab_max_width = 32
 config.show_new_tab_button_in_tab_bar = false
 config.status_update_interval = 500
 config.window_decorations = 'NONE'
+config.adjust_window_size_when_changing_font_size = false
 config.colors.tab_bar = config.colors.tab_bar or {}
 config.colors.tab_bar.background = require('tabline.config').theme.normal_mode.c.bg
 
